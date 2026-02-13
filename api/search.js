af068a67d88fe4d23987dd5ad69224fc7f64e180
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-13 13:55:04 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,17 +1,14 @@
  export default async function handler(req, res) {
-  const TOKEN = process.env.TMDB_TOKEN;
-  const query = req.query.query;
+  try {
+    const query = req.query.query;
 
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${query}`,
-    {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-        "Content-Type": "application/json"
-      }
-    }
-  );
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=cc9374659de08b939499a50af4715216&query=${query}`
+    );
 
-  const data = await response.json();
-  res.status(200).json(data);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Search failed" });
+  }
 }
