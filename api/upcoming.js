9f5c6c6b9286d39f44ae1f3c4eaecab9474d60c7
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-13 10:38:59 +0530

    Create upcoming.js

---
+++ api/upcoming.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const TOKEN = process.env.TMDB_TOKEN;
+
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/upcoming",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
