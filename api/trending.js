0e9a325f0ddcee7cc28849a5749487d6a13dfedd
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-13 13:53:39 +0530

    Update trending.js

--- api/trending.js
+++ api/trending.js
@@ -1,16 +1,12 @@
  export default async function handler(req, res) {
-  const TOKEN = process.env.TMDB_TOKEN;
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/trending/movie/week?api_key=cc9374659de08b939499a50af4715216"
+    );
 
-  const response = await fetch(
-    "https://api.themoviedb.org/3/trending/movie/week",
-    {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-        "Content-Type": "application/json"
-      }
-    }
-  );
-
-  const data = await response.json();
-  res.status(200).json(data);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch trending movies" });
+  }
 }
