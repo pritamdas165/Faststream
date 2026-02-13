62e89250c35e646ea477592953bd2c1fcdd5a462
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-13 10:37:56 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,21 +1,17 @@
-export default async function handler(req, res) {
+ export default async function handler(req, res) {
   const TOKEN = process.env.TMDB_TOKEN;
-  const { query } = req.query;
+  const query = req.query.query;
 
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${query}`,
-      {
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          "Content-Type": "application/json"
-        }
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${query}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json"
       }
-    );
+    }
+  );
 
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
+  const data = await response.json();
+  res.status(200).json(data);
 }
