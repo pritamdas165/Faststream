058b7f8cd79562d26483d2e4bf58b144c05dc94f
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-13 10:37:24 +0530

    Update trending.js

--- api/trending.js
+++ api/trending.js
@@ -1,25 +1,16 @@
-export default async function handler(req, res) {
+ export default async function handler(req, res) {
+  const TOKEN = process.env.TMDB_TOKEN;
 
-const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTJkMzRjZDVmYTQ3ZTFkZjE4YTNkZTg3MGZhOGM3NCIsIm5iZiI6MTc3MDkwMTExNy41MzYsInN1YiI6IjY5OGRjZTdkZjQ5NjQ5NjdhZjc4ZGIyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jQYrmuv7kAL4s-g82qXBk4W9Lzc6Pi8xc4TwB7z69Fk";
+  const response = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/week",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
 
-try {
-
-const response = await fetch(
-"https://api.themoviedb.org/3/trending/movie/week",
-{
-headers: {
-Authorization: `Bearer ${TOKEN}`,
-"Content-Type": "application/json"
-}
-}
-);
-
-const data = await response.json();
-
-res.status(200).json(data);
-
-} catch (error) {
-res.status(500).json({ error: "Failed to fetch data" });
-}
-
+  const data = await response.json();
+  res.status(200).json(data);
 }
