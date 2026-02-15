42e3ac39b7c809accec80027dbbc594993594a27
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-15 09:11:19 +0530

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,8 +1,11 @@
-export default async function handler(req, res) {
+ export default async function handler(req, res) {
   const { type } = req.query;
 
+  if (!type) {
+    return res.status(400).json({ error: "Type is required" });
+  }
+
   const TMDB_KEY = "11a35ddf80fa31fe5fddf8087e313018";
- // <-- এখানে তোমার v3 key
   const url = `https://api.themoviedb.org/3/${type}?api_key=${TMDB_KEY}`;
 
   try {
