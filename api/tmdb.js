65a76d4b9eb4b45c21c3474b0fb9878333a9340e
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-16 14:23:53 +0530

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,11 +1,13 @@
- // api/tmdb.js
+// api/tmdb.js
+
 const API_KEY = "cc9374659de08b939499a50af4715216";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function getPopularMovies() {
-  const res = await fetch(
+  const response = await fetch(
     `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
   );
-  const data = await res.json();
+
+  const data = await response.json();
   return data.results;
 }
