85f55916e65bc41ff18b00d453592e63e786112b
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-16 14:24:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import{ getPopularMovies } from "../api/tmdb.js";
+import { getPopularMovies } from "../api/tmdb.js";
 
 const movieContainer = document.getElementById("movie-container");
 
@@ -7,20 +7,19 @@ async function loadMovies() {
   movieContainer.innerHTML = "";
 
   movies.forEach(movie => {
-    const movieDiv = document.createElement("div");
-    movieDiv.classList.add("movie");
+    const div = document.createElement("div");
+    div.className = "movie";
 
-    movieDiv.innerHTML = `
+    div.innerHTML = `
       <img src="${
-  movie.poster_path
-    ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-    : 'https://via.placeholder.com/300x450?text=No+Image'
-}">
-
+        movie.poster_path
+          ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+          : "https://via.placeholder.com/300x450?text=No+Image"
+      }">
       <h3>${movie.title}</h3>
     `;
 
-    movieContainer.appendChild(movieDiv);
+    movieContainer.appendChild(div);
   });
 }
 
