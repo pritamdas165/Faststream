865623bdf8253d7a43aba77036bc9a25eec8647f
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-16 14:58:11 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,24 +3,29 @@ import { getPopularMovies } from "./tmdb.js";
 const movieContainer = document.getElementById("movie-container");
 
 async function loadMovies() {
-  const movies = await getPopularMovies();
-  movieContainer.innerHTML = "";
+  try {
+    const movies = await getPopularMovies();
+    movieContainer.innerHTML = "";
 
-  movies.forEach(movie => {
-    const div = document.createElement("div");
-    div.className = "movie";
+    movies.forEach(movie => {
+      const div = document.createElement("div");
+      div.className = "movie";
 
-    div.innerHTML = `
-      <img src="${
-        movie.poster_path
-          ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-          : "https://via.placeholder.com/300x450?text=No+Image"
-      }">
-      <h3>${movie.title}</h3>
-    `;
+      div.innerHTML = `
+        <img src="${
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+            : "https://via.placeholder.com/300x450?text=No+Image"
+        }" />
+        <h3>${movie.title}</h3>
+      `;
 
-    movieContainer.appendChild(div);
-  });
+      movieContainer.appendChild(div);
+    });
+  } catch (error) {
+    movieContainer.innerHTML = "<p>Error loading movies</p>";
+    console.error(error);
+  }
 }
 
 loadMovies();
